0462299cdbd5bef8b9a7d4e6e75e34dc8a1b9fb1
Luca Morandini <lmorandini@ieee.org>
2018-05-24 16:04:35 +1000

    - Added compiletemplate task #2

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 'use strict';
 
 const _ = require('underscore');
+const ejs = require('ejs');
+const os = require('os');
+const uniqueFilename = require('unique-filename')
 
 /**
  * Openstack options that take the single dash
  * @type {string[]}
  */
-const openstackSingleDashOptions = require('./openstack-singledash.json');
+const openstackSingleDashOptions = require('./commands-templates/openstack-singledash.json');
 
 /**
- * Options for different OpenStack commands
+ * Options for different OpenStack clients
  */
-const openstack_commands = require('./openstack-commands.json');
+const openstack_commands = require('./commands-templates/openstack-commands.json');
+const heat_commands = require('./commands-templates/heat-commands.json');
 
 /**
  * Converts an array of obkects to double-dashed CLI options
@@ -29,10 +33,89 @@ const optionsToCLISwitches = ({optionsArr, singleDashOptions = openstackSingleDa
 };
 
 /**
+ * Retuns Grunt CLI options as an object
+ * @param grunt Grunt object
+ * @returns {Object}
+ */
+const getGruntCLIOptions = (grunt) => {
+  const fRE = /(\-+)(.+)/;
+  const vRE = /(\-+)(.+)=(.+)/;
+
+  const keys = _.map(_.values(grunt.option.flags()), (v) => {
+    if (vRE.test(v)) {
+      return v.match(vRE)[2];
+    }
+    if (fRE.test(v)) {
+      return v.match(fRE)[2];
+    }
+    return null;
+  });
+  const values = _.map(_.values(grunt.option.flags()), (v) => {
+    if (vRE.test(v)) {
+      return v.match(vRE)[3];
+    }
+    if (fRE.test(v)) {
+      return "";
+    }
+    return null;
+  });
+
+  return _.object(keys, values)
+};
+
+module.exports.json2Yaml = ({grunt, input, output, callback}) => {
+  const f = uniqueFilename(os.tmpdir()) + '.json';
+  return {
+    command: () => {
+      console.log(`******* ~/git/json-file-import/bin/jsonimport ${input} > ${f} && json2yaml ${f} > ${output}`); // XXX
+      return `~/git/json-file-import/bin/jsonimport ${input} > ${f} && json2yaml ${f} > ${output}`;
+    },
+    stdout: _.isUndefined(grunt.option('verbose')) ? false : grunt.option('verbose'),
+    callback: _.isFunction(callback) ? callback :
+      (error, stdout, stderr) => {
+        if (error) {
+          grunt.log.error(error);
+        } else {
+          grunt.log.ok(stdout);
+        }
+      }
+  }
+};
+
+/**
+ * Returns an Object of Grunt tasks: one for every command in openstack_commands
+ */
+module.exports.openstack = _.mapObject(openstack_commands,
+  (val, key) => {
+    return ({grunt, globalOptions, options = {}, output, callback}) => {
+      return executeTask({
+        grunt, client: 'openstack', obj: val[0], cmd: val[1] ? val[1] : "",
+        globalOptions, options, output, callback
+      });
+    }
+  }
+);
+
+/**
+ * Returns an Object of Grunt tasks: one for every command in heat_commands
+ */
+module.exports.heat = _.mapObject(heat_commands,
+  (val, key) => {
+    return ({grunt, globalOptions, options = {}, output, callback}) => {
+      return executeTask({
+        grunt, client: 'heat', obj: val[0], cmd: val[1] ? val[1] : "",
+        globalOptions, options, output, callback
+      });
+    }
+  }
+);
+
+/**
  * Returns the options to execute a task in grunt-exec and either a callback
  * or an output type
  *
  * @param grunt {Object} Grunt singleton
+ * @param client {String} CLI to call
  * @param obj {String} Object the action acts upom
  * @param cmd {String} Command to execute
  * @param globalOptions {Object} Gloabl options (mainly Authorization)
@@ -41,43 +124,31 @@ const optionsToCLISwitches = ({optionsArr, singleDashOptions = openstackSingleDa
  * @param callback {Function} Function to execute upon commmand termination
  * @returns {{cmd: (function(): string), stdout: boolean, callback: Function}}
  */
-const executeOSTask = ({grunt, obj, cmd, globalOptions, options = {}, output = 'table', callback}) => {
-    const debugFlag = _.isUndefined(grunt.option('verbose')) ? false : grunt.option('verbose');
-    const cmdString = ['openstack'].concat(
-      optionsToCLISwitches({optionsArr: [globalOptions]})).concat(
-      [obj, cmd]).concat(
-      optionsToCLISwitches({optionsArr: [options, {f: _.isFunction(callback) ? 'json' : output}]}))
-      .concat([grunt.option('verbose') ? '--debug' : '']).join(' ');
-    if (debugFlag) {
-      grunt.log.ok(`>>>> COMMAND: ${cmdString}`);
-    }
-    return {
-      command: () => {
-        return cmdString;
-      },
-      stdout: debugFlag,
-      callback: _.isFunction(callback) ? callback :
-        (error, stdout, stderr) => {
-          if (error) {
-            grunt.log.error(error);
-          } else {
-            grunt.log.ok(stdout);
-          }
-        }
-    }
-  }
-;
+const executeTask = ({grunt, client, obj, cmd, globalOptions, options = {}, output = 'table', callback}) => {
+  let cmdString = [client].concat(
+    optionsToCLISwitches({optionsArr: [globalOptions]})).concat(
+    [obj, cmd]).concat(
+    optionsToCLISwitches({optionsArr: [options, {f: output}]}))
+    .concat([grunt.option('verbose') ? '--debug' : '']).join(' ');
+  cmdString = ejs.render(cmdString, {
+    options: _.extend(_.extend(_.clone(options), globalOptions),
+      getGruntCLIOptions(grunt))
+  });
 
-/**
- * Returns an Object of Grunt tasks: one for every command in openstack_commands
- */
-module.exports = _.mapObject(openstack_commands,
-  (val, key) => {
-    return ({grunt, globalOptions, options = {}, output, callback}) => {
-      return executeOSTask({
-        grunt, obj: val[0], cmd: val[1] ? val[1] : "",
-        globalOptions, options, output, callback
-      });
-    }
+  return {
+    command: () => {
+      return cmdString;
+    },
+    stdout: _.isUndefined(grunt.option('verbose')) ? false : grunt.option('verbose'),
+    callback: _.isFunction(callback) ? callback :
+      (error, stdout, stderr) => {
+        grunt.log.ok(`${cmdString}`);
+
+        if (error) {
+          grunt.log.error(error);
+        } else {
+          grunt.log.ok(stdout);
+        }
+      }
   }
-);
+};
